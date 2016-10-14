0d89a5ad07bdf442d7754358bb183274b220d92c
Khushpreet <khushpreet-kaur@users.noreply.github.com>
2016-10-14 17:11:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('article1', function (req, res) {
+  res.sendFile('Article one is requested');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
